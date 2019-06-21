a7a3441d3d5cef22a374cfd1ef1bee81bbe28886
pabloandresito <pabloandresito@gmail.com>
2019-06-21 15:08:04 -0500

    Form: Ingresar versión #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pablo Parking';
+  textError = "";
+  textRetirar = "";
+
+  registroParqueoDto = {
+    placa:null,
+    cilindraje:null,
+    tipoVehiculo:null
+  };
+
+  ingresar() {
+
+  };
 }
